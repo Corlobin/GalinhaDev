3fbdfc7dd17ffcdc099a49efb02703737277b782
Ricardo Brasil <corlobin@gmail.com>
2015-09-21 12:55:21 -0300

    Implementando as funcionalidades OO

---
+++ TelaCenario.py
@@ -0,0 +1,45 @@
+__author__ = 'Ricardo'
+
+import pygame
+
+from Player import Player
+from Imagem import Imagem
+
+
+class TelaCenario(object):
+    def __init__(self):
+        # Iniciais
+        self.backgroundJogo = Imagem.load_image('fundoteste.png', 0)
+        self.backgroundJogo = pygame.transform.scale(self.backgroundJogo, (800, 500))
+        self.galinha = Player("galinhaAndando.gif", 408, 5)
+        self.backLocal = 0
+        return
+
+    def mostraFase1(self, game):
+        for evento in pygame.event.get():
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_RIGHT:
+                    self.galinha.moveDireita()
+                    print("direita", self.galinha.pos.right)
+                    self.moveCenario1()
+                if evento.key == pygame.K_LEFT:
+                    self.galinha.moveEsquerda()
+                    print("esquerda", self.galinha.pos.left)
+
+            if evento.type == pygame.QUIT:
+                game.status = 2
+
+        game.screen.blit(self.backgroundJogo, (self.backLocal, 0))
+        game.screen.blit(self.galinha.image, self.galinha.pos)
+        pygame.display.update()
+        return
+
+    def moveCenario1(self):
+        if (self.galinha.pos.right <= 1500):
+            self.backLocal -= self.galinha.speed * 2
+        return
+
+    def moveCenario2(self):
+        if ((self.galinha.pos.right - self.galinha.pos.right) >= 32):
+            self.backLocal += 5
+        return
---
+++ TelaMenu.py
@@ -0,0 +1,63 @@
+__author__ = 'Ricardo'
+
+import pygame
+
+from Player import Player
+from Imagem import Imagem
+
+
+class TelaMenu(object):
+    def __init__(self):
+
+        self.posicaoCirculo = 320
+        self.backgroundMenu = Imagem.load_image('panel.png', 0)
+        print("background 1...")
+
+        self.backgroundMenu = pygame.transform.scale2x(self.backgroundMenu)
+        self.backgroundMenu = pygame.transform.scale2x(self.backgroundMenu)
+        print("background 2...")
+        self.galinhaAndandoMenu1 = Player("galinhaAndando.gif", 120, 1)
+        self.galinhaAndandoMenu2 = Player("galinhaAndando.gif", 180, 1)
+        self.fonte = pygame.font.SysFont("comicsansms", 30)
+        self.textoIniciar = self.fonte.render("Iniciar", 1, (255, 255, 225))
+        self.textoSair = self.fonte.render("Sair", 1, (255, 255, 225))
+        return
+
+    def capturarEventos(self, game):
+        for evento in pygame.event.get():
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_UP:  # PARA CIMA
+                    if self.posicaoCirculo == 360:
+                        self.posicaoCirculo = 320
+
+                if evento.key == pygame.K_DOWN:  # PARA BAIXO
+                    if self.posicaoCirculo == 320:
+                        self.posicaoCirculo = 360
+
+                if evento.key == 13:  # KEY ENTER
+                    print("Enter")
+                    if self.getOpcao() == 1:  # Iniciar
+                        game.status = 1
+                        print("iniciar")
+                    else:  # sair
+                        game.status = 2
+
+            if evento.type == pygame.QUIT:
+                game.status = 2
+
+        game.screen.blit(self.backgroundMenu, (0, 0))
+        self.galinhaAndandoMenu1.moveDireita()
+        self.galinhaAndandoMenu2.moveDireita()
+
+        game.screen.blit(self.galinhaAndandoMenu1.image, self.galinhaAndandoMenu1.pos)
+        game.screen.blit(self.galinhaAndandoMenu2.image, self.galinhaAndandoMenu2.pos)
+        game.screen.blit(self.textoIniciar, (300, 300))
+        game.screen.blit(self.textoSair, (300, 340))
+        pygame.draw.circle(game.screen, (255, 255, 255), (285, self.posicaoCirculo), 5, 0)
+        pygame.display.update()
+        return
+
+    def getOpcao(self):
+        if self.posicaoCirculo == 320:
+            return 1
+        return 0
